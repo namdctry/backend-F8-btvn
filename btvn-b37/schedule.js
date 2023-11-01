9c5469c926b5f1c9307a91b5f8e9ae51dfdc7203
nguyen nam <nam2002bv@gmail.com>
2023-11-01 15:05:45 +0700

    chua xong dau

--- btvn-b37/schedule.js
+++ btvn-b37/schedule.js
@@ -1,7 +1,7 @@
 var cron = require("node-cron");
 const model = require("./models/index");
 const QueueJob = model.queueJob;
-const sendMail = require("./jobs/SendMail");
+const SendMail = require("./jobs/SendMail");
 cron.schedule("*/5 * * * * *", async () => {
   console.log("running a task every 5 seconds");
   console.log(222);
@@ -10,7 +10,9 @@ cron.schedule("*/5 * * * * *", async () => {
   });
   const { name, email } = JSON.parse(job.value).data.job;
   console.log(name, email);
-  new SendMail({ name, email });
-  await job.destroy();
-  console.log("544564");
+  new SendMail({ name, email }).handle();
+  await QueueJob.destroy({
+    where: { id: 1 },
+  });
+  console.log(22214654646);
 });
